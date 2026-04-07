2f38ae31b820d8dc835e8cd7b3c0b0b74816cc43
Olugbemi <olugbemimoses13@gmail.com>
2026-04-07 05:24:50 +0100

    Implemented full website structure and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 
+import { Route, Routes } from 'react-router'
 import './App.css'
-import HomePage from './pages/Homepage'
+import HomePage from './pages/homepage/HomePage'
+import CheckoutPage from './pages/checkout/CheckoutPage'
+import TrackingPage from './pages/tracking/TrackingPage'
+import OrdersPage from './pages/orders/OrdersPage'
 
 
 function App() {
  
 
   return (
-  <HomePage/>
+ <Routes>
+  <Route path='/' element={<HomePage/>}></Route>
+  <Route path='checkout' element={<CheckoutPage/>}></Route>
+  <Route path='tracking' element={<TrackingPage/>}></Route>
+  <Route path='orders' element={<OrdersPage/>}></Route>
+
+ </Routes>
    
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,41 @@
+import { Link, NavLink } from 'react-router';
+import './header.css'
+
+
+const Header = () => {
+
+    return(
+           <div class="header">
+      <div class="left-section">
+        <NavLink to="/" class="header-link">
+          <img class="logo"
+            src="images/logo-white.png" />
+          <img class="mobile-logo"
+            src="images/mobile-logo-white.png" />
+        </NavLink>
+      </div>
+
+      <div class="middle-section">
+        <input class="search-bar" type="text" placeholder="Search" />
+
+        <button class="search-button">
+          <img class="search-icon" src="images/icons/search-icon.png" />
+        </button>
+      </div>
+
+      <div class="right-section">
+        <NavLink class="orders-link header-link" to="/orders" >
+          <span class="orders-text">Orders</span>
+        </NavLink>
+
+        <NavLink class="cart-link header-link" to="/checkout">
+          <img class="cart-icon" src="images/icons/cart-icon.png" />
+          <div class="cart-quantity">3</div>
+          <div class="cart-text">Cart</div>
+        </NavLink>
+      </div>
+    </div>
+    )
+};
+
+export default Header
