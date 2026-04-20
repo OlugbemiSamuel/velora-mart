4f296627650d6e1569dceb53d688b59f230611f2
Olugbemi <olugbemimoses13@gmail.com>
2026-04-20 19:51:01 +0100

    feat: Implemented integration test for products component

--- velora-mart/src/pages/homepage/Products.jsx
+++ velora-mart/src/pages/homepage/Products.jsx
@@ -29,7 +29,7 @@ const Products = ({ product, getCartItems }) => {
   return (
     <div className="product-container">
       <div className="product-image-container">
-        <img className="product-image" src={product.image} />
+        <img className="product-image" data-testId = 'product-image' src={product.image} />
       </div>
 
       <div className="product-name limit-text-to-2-lines">{product.name}</div>
@@ -37,9 +37,10 @@ const Products = ({ product, getCartItems }) => {
       <div className="product-rating-container">
         <img
           className="product-rating-stars"
+          data-testId= 'product-rating-stars'
           src={`images/ratings/rating-${product.rating.stars * 10}.png`}
         />
-        <div className="product-rating-count link-primary">
+        <div className="product-rating-count link-primary " data-testId='product-rating-count'>
           {product.rating.count}
         </div>
       </div>
--- velora-mart/src/utils/money.test.js
+++ velora-mart/src/utils/money.test.js
@@ -1,18 +1,13 @@
-import {it, expect, describe} from 'vitest';
-import { formatMoney } from './Money';
+import { it, expect, describe } from "vitest";
+import { formatMoney } from "./Money";
 
-describe('formatMoney', () => {
-     
-it('formats 2020 cents to $20.20', () => {
-    expect(formatMoney(2020)).toBe('$20.20');
+describe("formatMoney", () => {
+  it("formats 2020 cents to $20.20", () => {
+    expect(formatMoney(2020)).toBe("$20.20");
+  });
+
+  it("checks for two decimals", () => {
+    expect(formatMoney(1000)).toBe("$10.00");
+    expect(formatMoney(0)).toBe("$0.00");
+  });
 });
-
-it('checks for two decimals', () => {
-    expect(formatMoney(1000)).toBe('$10.00');
-    expect(formatMoney(0)).toBe('$0.00')
-})
-
-
-})
-
-
